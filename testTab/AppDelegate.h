c7bb5a3ae65c9bf4dbe38a41aa2b28b5d843e841
yiyi <yiban@611611.local>
2015-08-13 15:37:19 +0800

    Initial Commit

---
+++ testTab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testTab
+//
+//  Created by yiban on 15/8/13.
+//  Copyright (c) 2015年 yiban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
